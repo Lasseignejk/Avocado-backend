7576b09ec1a1eef93339c2dbb4d357a4743d55bd
Lasseignejk <jaye914@hotmail.com>
2023-02-28 15:04:25 -0500

    light updates to index.js

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const products = require('./products');
-import { createClient } from "@supabase/supabase-js";
+const express = require("express");
+const cors = require("cors");
+const products = require("./products");
+
 const PORT = process.env.PORT || 3060;
-const cloudinary = require('cloudinary')
-const supabaseUrl = 'https://dwjnomervswgqasgexck.supabase.co'
-const supabaseKey = import.meta.env.VITE_KEY;
+const cloudinary = require("cloudinary");
+const supabaseUrl = "https://dwjnomervswgqasgexck.supabase.coM";
+const supabaseKey = env.VITE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
 });
 app.get("/products", (req, res) => {
-  res.send(products);
+	res.send(products);
 });
 
 app.post("/products", async (req, res) => {
-  const { data, error } = await supabase.auth.signUp({
-    email: req.body.email,
-    password: req.body.password,
-  });
-})
-
-
-
-
-
-
-
-
-
+	const { data, error } = await supabase.auth.signUp({
+		email: req.body.email,
+		password: req.body.password,
+	});
+});
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
