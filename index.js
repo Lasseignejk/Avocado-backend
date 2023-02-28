ece8cb5536abdba810c39c036fdaf97f83270f28
Megan <meganthielman@gmail.com>
2023-02-28 12:11:07 -0800

    Index.js updates

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const products = require('./products');
+const express = require("express");
+const cors = require("cors");
+const products = require("./products");
 import { createClient } from "@supabase/supabase-js";
 const PORT = process.env.PORT || 3060;
-const cloudinary = require('cloudinary')
-const supabaseUrl = 'https://dwjnomervswgqasgexck.supabase.co'
+const cloudinary = require("cloudinary");
+const supabaseUrl = "https://dwjnomervswgqasgexck.supabase.co";
 const supabaseKey = import.meta.env.VITE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.get("/products", (req, res) => {
-  res.send(products);
+
+app.get("/user", (req, res) => {
+  res.send(user);
 });
 
 app.post("/products", async (req, res) => {
@@ -23,15 +21,6 @@ app.post("/products", async (req, res) => {
     email: req.body.email,
     password: req.body.password,
   });
-})
-
-
-
-
-
-
-
-
-
+});
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
