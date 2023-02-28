6fbf4c89436f3bc5983a918dc839c33b84e8d284
corewill <118204159+corewill@users.noreply.github.com>
2023-02-28 14:43:18 -0500

    midday

--- index.js
+++ index.js
@@ -7,15 +7,51 @@ const cloudinary = require('cloudinary')
 const supabaseUrl = 'https://dwjnomervswgqasgexck.supabase.co'
 const supabaseKey = import.meta.env.VITE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/products", (req, res) => {
-  res.send(products);
-});
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+ let { data, error } = await supabase.auth.signUp({
+  email: req.body.email,
+  password: req.body.password,
+})
+
+
+
+console.log(error)
+    if(data){
+      res.send(data)
+      return
+    } else {
+      res.send("Error signing up")
+    }
+})
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const {user, error} = await supabase.auth.signInWithPassword({ 
+      email: req.body.email,
+      password: req.body.password,  
+     })
+    console.log(error)
+    
+    if(user.data){
+      res.render("pages/signedin")
+      return
+    }}
+   catch (error) {
+
+      res.status(400).send(error)
+
+  }})
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
