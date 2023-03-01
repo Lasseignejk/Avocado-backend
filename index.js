85e64a8bb8b8970d7899e3968a935c4c4a01c910
Megan <meganthielman@gmail.com>
2023-03-01 08:48:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,74 +13,131 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
+//signup route
 app.post("/signup", async (req, res) => {
-	const {
-		CustomerEmail,
-		Password,
-		CustomerFirstName,
-		CustomerLastName,
-		CustomerPhoneNumber,
-		Address,
-		RestOwner,
-	} = req.body;
+  const {
+    CustomerEmail,
+    Password,
+    CustomerFirstName,
+    CustomerLastName,
+    CustomerPhoneNumber,
+    Address,
+    RestOwner,
+  } = req.body;
 
-	await supabase.auth.signUp({
-		email: CustomerEmail,
-		password: Password,
-	});
+  await supabase.auth.signUp({
+    email: CustomerEmail,
+    password: Password,
+  });
 
-	if (RestOwner == "false") {
-		let { data, error } = await supabase.from("Customer").insert([
-			{
-				CustomerFirstName: CustomerFirstName,
-				CustomerLastName: CustomerLastName,
-				CustomerEmail: CustomerEmail,
-				CustomerPhoneNumber: CustomerPhoneNumber,
-				Address: Address,
-			},
-		]);
-		console.log(data);
-		console.log(error);
-	} else {
-		let { data, error } = await supabase.from("Owner").insert([
-			{
-				OwnerFirstName: CustomerFirstName,
-				OwnerLastName: CustomerLastName,
-				OwnerEmail: CustomerEmail,
-				OwnerPhoneNumber: CustomerPhoneNumber,
-			},
-		]);
-		console.log(data);
-		console.log(error);
-	}
+  if (RestOwner == "false") {
+    let { data, error } = await supabase.from("Customer").insert([
+      {
+        CustomerFirstName: CustomerFirstName,
+        CustomerLastName: CustomerLastName,
+        CustomerEmail: CustomerEmail,
+        CustomerPhoneNumber: CustomerPhoneNumber,
+        Address: Address,
+      },
+    ]);
+    console.log(data);
+    console.log(error);
+  } else {
+    let { data, error } = await supabase.from("Owner").insert([
+      {
+        OwnerFirstName: CustomerFirstName,
+        OwnerLastName: CustomerLastName,
+        OwnerEmail: CustomerEmail,
+        OwnerPhoneNumber: CustomerPhoneNumber,
+      },
+    ]);
+    console.log(data);
+    console.log(error);
+  }
 });
 
+//rest information route
 app.post("/addrest", async (req, res) => {
-	const {
-		RestName,
-		RestLocation,
-		RestPhoneNumber,
-		RestHours,
-		RestLogo,
-		OwnerId,
-	} = req.body;
-	let { data, error } = await supabase.from("Restaurant").insert([
-		{
-			RestName: RestName,
-			RestLocation: RestLocation,
-			RestPhoneNumber: RestPhoneNumber,
-			RestHours: RestHours,
-			RestLogo: RestLogo,
-			OwnerId: OwnerId,
-		},
-	]);
-	console.log(data);
-	console.log(error);
+  const {
+    RestName,
+    RestLocation,
+    RestPhoneNumber,
+    RestHours,
+    RestLogo,
+    OwnerId,
+  } = req.body;
+  let { data, error } = await supabase.from("Restaurant").insert([
+    {
+      RestName: RestName,
+      RestLocation: RestLocation,
+      RestPhoneNumber: RestPhoneNumber,
+      RestHours: RestHours,
+      RestLogo: RestLogo,
+      OwnerId: OwnerId,
+    },
+  ]);
+  console.log(data);
+  console.log(error);
+});
+
+//update routes - Owner
+app.post("/update", async (req, res) => {
+  const {
+    CustomerEmail,
+    CustomerFirstName,
+    CustomerLastName,
+    CustomerPhoneNumber,
+  } = req.body;
+
+  const { data, error } = await supabase
+    .from("Owner")
+    .update({
+      id: CustomerFirstName,
+      OwnerLastName: CustomerLastName,
+      OwnerEmail: CustomerEmail,
+      OwnerPhoneNumber: CustomerPhoneNumber,
+    })
+    .eq(
+      id,
+      CustomerFirstName,
+      OwnerLastName,
+      CustomerLastName,
+      OwnerEmail,
+      CustomerEmail,
+      OwnerPhoneNumber,
+      CustomerPhoneNumber
+    );
+});
+
+//update routes - Customer
+app.post("/update", async (req, res) => {
+  const {
+    CustomerEmail,
+    CustomerFirstName,
+    CustomerLastName,
+    CustomerPhoneNumber,
+  } = req.body;
+
+  const { data, error } = await supabase
+    .from("Customer")
+    .update({
+      id: CustomerFirstName,
+      CustomerLastName: CustomerLastName,
+      CustomerEmail: CustomerEmail,
+      CustomerPhoneNumber: CustomerPhoneNumber,
+    })
+    .eq(
+      id,
+      CustomerFirstName,
+      CustomerLastName,
+      CustomerEmail,
+      CustomerPhoneNumber
+    );
 });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
