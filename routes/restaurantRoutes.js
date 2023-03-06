05b4bb67ce10dbb2c8b6b39e58bc1ac935f740e3
Lasseignejk <jaye914@hotmail.com>
2023-03-06 17:12:08 -0500

    added route to find one restaurant by rest id

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -11,77 +11,94 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 // MIDDLEWARE
 router.use(cors());
 router.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 );
 router.use(bodyParser.json());
 
 // get restaurants by id of admin
 router.get("/getrestaurants", async (req, res) => {
-  userid = req.headers.userid;
-  // console.log(req.get(userId));
-  const { data, error } = await supabase
-    .from("Restaurant")
-    .select()
-    .eq("OwnerId", userid);
+	userid = req.headers.userid;
+	// console.log(req.get(userId));
+	const { data, error } = await supabase
+		.from("Restaurant")
+		.select()
+		.eq("OwnerId", userid);
 
-  if (error) {
-    res.send(error);
-  }
-  if (data) {
-    res.send(data);
-  }
+	if (error) {
+		res.send(error);
+	}
+	if (data) {
+		res.send(data);
+	}
 });
+
+// get a restaurant by restaurant id
+router.get("/getonerestaurant", async (req, res) => {
+	restid = req.headers.restid;
+	const { data, error } = await supabase
+		.from("Restaurant")
+		.select()
+		.eq("id", restid);
+
+	if (error) {
+		res.send(error);
+	}
+	if (data) {
+		res.send(data);
+	}
+});
+
 // get menuitems by restaurant id
 
 router.get("/getmenu", async (req, res) => {
-  restid = req.headers.restid;
-  const { data, error } = await supabase
-    .from("MenuItems")
-    .select()
-    .eq("RestId", restid);
+	restid = req.headers.restid;
+	const { data, error } = await supabase
+		.from("MenuItems")
+		.select()
+		.eq("RestId", restid);
 
-  if (error) {
-    res.send(error);
-  }
-  if (data) {
-    res.send(data);
-    console.log(data);
-  }
+	if (error) {
+		res.send(error);
+	}
+	if (data) {
+		res.send(data);
+		console.log(data);
+	}
 });
 
 // add restaurant
 router.post("/addrest", async (req, res) => {
-  const {
-    RestName,
-    RestLocation,
-    RestPhoneNumber,
-    RestHours,
-    RestLogo,
-    OwnerId,
-  } = req.body;
-  let { data, error } = await supabase.from("Restaurant").insert([
-    {
-      RestName: RestName,
-      RestLocation: RestLocation,
-      RestPhoneNumber: RestPhoneNumber,
-      RestHours: RestHours,
-      RestLogo: RestLogo,
-      OwnerId: OwnerId,
-    },
-  ]);
-  res.send(data);
+	const {
+		RestName,
+		RestLocation,
+		RestPhoneNumber,
+		RestHours,
+		RestLogo,
+		OwnerId,
+	} = req.body;
+	let { data, error } = await supabase.from("Restaurant").insert([
+		{
+			RestName: RestName,
+			RestLocation: RestLocation,
+			RestPhoneNumber: RestPhoneNumber,
+			RestHours: RestHours,
+			RestLogo: RestLogo,
+			OwnerId: OwnerId,
+		},
+	]);
+	res.send(data);
 });
 
 // get all restaurants
 router.get("/displayrest", async (req, res) => {
-  let { data: Restaurant, error } = await supabase
-    .from("Restaurant")
-    .select("*");
-  console.log(Restaurant);
-  console.log(error);
-  res.send(Restaurant);
+	let { data: Restaurant, error } = await supabase
+		.from("Restaurant")
+		.select("*");
+	console.log(Restaurant);
+	console.log(error);
+	res.send(Restaurant);
 });
 
 module.exports = router;
