44940427f47bdb6a61db5c2faa15a88fdb5b8747
Lasseignejk <jaye914@hotmail.com>
2023-03-07 11:58:06 -0500

    updated index.js
    still working on deployment, res was not defined

--- index.js
+++ index.js
@@ -133,6 +133,8 @@ app.get("/displayorderitem", async (req, res) => {
 	res.send(Order);
 });
 
-res.send("Hello!");
+server.use((req, res) => {
+	res.send("Hello World");
+});
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
